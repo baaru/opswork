33e4fa208022d6fe9b0fc00c35035307c7d541e0
bharathi <bharathi1086304@gmail.com>
2020-03-10 07:29:40 +0000

    second commit

--- infy-C/recipes/infy-R.rb
+++ infy-C/recipes/infy-R.rb
@@ -16,4 +16,13 @@ end
 package "tree" do
   action :install
 end
-
+file '/robofile' do
+  content "This is to get Attributes
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']}
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}"
+  owner 'root'
+  group 'root'
+  action :create
+end
